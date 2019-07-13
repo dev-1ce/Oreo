ce758ba15a3457e480fdc425f50d9d3470e12615
abhinav_21674 <abhinav.pandey@paytm.com>
2019-07-13 17:47:05 +0530

    A fresh start re-wrote history to clear openode. config form commit history

--- app.js
+++ app.js
@@ -4,36 +4,38 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var compression = require('compression')
+var data = require("./data");
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+// var index = require('./routes/index');
+// var users = require('./routes/users');
+// var config = require('./config/config');
+const request = require('request-promise');
+
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views2'));
+app.set('view engine', 'ejs');
+var Nexmo = require('nexmo');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(compression());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), { cacheControl: true, maxAge: '1 days' }));
+app.use(logger('dev'));
 
 // app.use('/', index);
 // app.use('/users', users);
 
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
+
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -44,29 +46,73 @@ app.use(function(err, req, res, next) {
 });
 
 
+// var nexmo = new Nexmo({
+//     apiKey: config.apiKey,
+//     apiSecret: config.apiSecret
+//     // applicationId: APP_ID,
+//     // privateKey: PRIVATE_KEY_PATH,
+//   },{});
+
+
 
 // app.get("/index",function (req,res) {
 // 	res.sendFile((path.join(__dirname,"views",'/index.html')));
 // });
 
-app.get("/ab", function (req,res) {
-  res.send("hey there");
-  //res.sendFile(path.join(path.join(__dirname,'views'),'/index.html'));
+
+app.get('/index', function (req, res) {
+  res.render('index');
 });
 
-app.get('/sample', function(req, res) {
-    res.send('this is a sample!');  
+app.get("/sitemap.xml", function (req, res) {
+  res.sendFile((path.join(__dirname, "views2", '/' + 'sitemap' + '.xml')))
 });
 
 
-app.get('/:name', function (req,res) {
-    name = req.params.name;
-    console.log(name);
-    res.sendFile((path.join(__dirname,"views",'/'+name+'.html')))
+
+app.get("/:name", (req, res) => {
+
+  var name = req.params.name;
+  console.log("req recieved with name" + name + "with data "  +data[name]);
+  if (data[name]) {
+    res.render('main',data[name]);
+  }
+  else
+    res.render('error');;
+  
 });
 
-app.listen(8080,function () {
-	console.log("server started at 8080");
+
+app.get("/sitemap.xml", function (req, res) {
+  res.sendFile((path.join(__dirname, "views", '/' + 'sitemap' + '.xml')));
+})
+
+
+app.get('/', function (req, res) {
+  console.log("got / route requested")
+  res.render('index');
+});
+
+
+
+// // commented portion for Serving static file with name as path of url present in views folder inside public
+// app.get('/:name', function (req, res) {
+//   name = req.params.name;
+//   console.log(name);
+//   res.sendFile((path.join(__dirname, "views", '/' + name + '.html')))
+// });
+
+
+
+// catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+
+app.listen(80, function () {
+  console.log("server started at 80");
 });
 
 
---
+++ data/index.js
@@ -0,0 +1,8 @@
+
+
+require('fs').readdirSync(__dirname + '/').forEach(function(file) {
+    if (file.match(/\.json$/) !== null && file !== 'index.js') {
+      var name = file.replace('.json', '');
+      exports[name] = require('./' + file);
+    }
+  });
