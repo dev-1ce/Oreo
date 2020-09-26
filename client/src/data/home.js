f8ac193f429ecffd0a0ed58ba90ee34f9fa271f9
Shruti3004 <mail2shruti.ag@gmail.com>
2020-09-26 16:43:12 +0530

    updated all the content

--- client/src/data/home.js
+++ client/src/data/home.js
@@ -6,7 +6,7 @@ export const DataHome = {
       imageBlog: "Pickup/Drop Rs-750",
       heading: "Book Cab At Varanasi Airport",
       paragraph:
-        "Babatpur 26 km northwest of Varanasi,Car Takes 45 mins to reach Airport from Varanasi, ie Get to Varanasi Airport at minimum cost. Get picked/Drop up From Varanasi/Babatpur Airport in Sedan at Rs 750-/ only. Hire Innova at Babatpur Airport at minimum Rate of Rs 950 immediately. With over 25 years of experience We guarantee 100% Customer Satisfaction Best Quality Cabs In Varanasi are provided at minimum cost.",
+        "Babatpur 26 km northwest of Varanasi,Car Takes 45 mins to reach Airport from Varanasi, i.e. Get to Varanasi Airport at minimum cost. Get pick/Drop up From Varanasi/Babatpur Airport in Sedan at Rs 750-/ only. Hire Innova at Babatpur Airport at minimum Rate of Rs 950 immediately. With over 25 years of experience We guarantee 100% Customer Satisfaction Best Quality Cabs In Varanasi are provided at minimum cost.",
       subHeading:
         "Hire cab at Lal Bahadur Shastri International Airport Varanasi at Minimum cost.",
       blogPost: [
@@ -39,7 +39,7 @@ export const DataHome = {
       imageBlog: "Culture",
       heading: "Book Car for Sarnath Darshan in Varanasi",
       paragraph:
-        "Buddha came to Sarnath to preach his message of the middle way to nirvana after he achieved enlightenment at Bodhgaya, and gave his famous first sermon here. In the 3rd century BC, Emperor Ashoka had magnificent stupas and monasteries erected here, as well as an engraved pillar. When Chinese traveller Xuan Zang dropped by in AD 640, Sarnath boasted a 100m-high stupa and 1500 monks living in large monasteries. However, soon after, Buddhism went into decline and, when Muslim invaders sacked the city in the late 12th century, Sarnath disappeared altogether. It was ‘rediscovered’ by British archaeologists in 1835.",
+        "Buddha came to Sarnath to preach his message of the middle way to nirvana after he achieved enlightenment at Bodhgaya, he gave his famous first sermon here. In the 3rd century BC, Emperor Ashoka had magnificent stupas and monasteries erected here, as well as an engraved pillar. When Chinese traveller Xuan Zang dropped by in AD 640, Sarnath boasted a 100m-high stupa and 1500 monks living in large monasteries. However, soon after, Buddhism went into decline and, when Muslim invaders sacked the city in the late 12th century, Sarnath disappeared altogether. It was ‘rediscovered’ by British archaeologists in 1835.",
       subHeading: "Cab rental for Sarnath tour At Rs 900",
       blogPost: [
         "4500+ pickups",
@@ -55,7 +55,7 @@ export const DataHome = {
       imageBlog: "Durga Temple In Varanasi",
       heading: "Famous Mandir to visit in Varanasi",
       paragraph:
-        "Durga Mandir (Hindi: दुर्गा मंदिर), also known as Durga Kund Mandir, Durga Temple and also monkey temple, is one of the most famous temples in the holy city of Varanasi. This temple has great religious importance in Hinduism and is dedicated to the Maa Durga. Durga Mandir was constructed in 18th century by Bengali Maharani (Bengali Queen",
+        "Durga Mandir (Hindi: दुर्गा मंदिर), also known as Durga Kund Mandir, Durga Temple and also monkey temple, is one of the most famous temples in the holy city of Varanasi. This temple has great religious importance in Hinduism and is dedicated to the Maa Durga. Durga Mandir was constructed in 18th century by Bengali Maharani (Bengali Queen)",
       subHeading: "Car rent for Durga Mandir Darshan Rs 800",
       blogPost: [
         "4500+ pickups",
@@ -67,4 +67,3 @@ export const DataHome = {
     },
   ],
 };
- 
