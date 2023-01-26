5d8689d775dc99a16ff274b36bd6736d6c951185
devabhinav <abhinavpandey.1996@gmail.com>
2023-01-26 14:21:15 +0530

    Changes in Rate in store front

--- app/src/data/home.js
+++ app/src/data/home.js
@@ -6,8 +6,8 @@ export const DataHome = {
       name: "Sedan",   
       paragraph:
       "5 seater, air-conditioned, top rated drivers, spacious. Enjoy extra comfy hassle free air conditioned ride.",
-      city: "1200",
-      outstation: "9",
+      city: "2000",
+      outstation: "10",
       airport: "750",
     },
     {
@@ -16,9 +16,9 @@ export const DataHome = {
       name: "Luxury Sedan",   
       paragraph:
       "5 seater, air-conditioned, top rated drivers, spacious. Enjoy extra comfy hassle free air conditioned ride.",
-      city: "2400",
-      outstation: "16",
-      airport: "1200",
+      city: "3500",
+      outstation: "18",
+      airport: "1500",
     },
     {
       id: 3,
@@ -26,9 +26,9 @@ export const DataHome = {
       name: "XUV",   
       paragraph:
       "7 seater, air-conditioned, extra luggage space, pocket friendly. Add comfort with quality. Enjoy extra space for you and your luggage as well.",
-      city: "1700",
-      outstation: "13",
-      airport: "900",
+      city: "2800",
+      outstation: "15",
+      airport: "1250",
     },
     {
       id: 4,
@@ -36,18 +36,18 @@ export const DataHome = {
       name: "Mini-Bus",   
       paragraph:
         "30 seater, economical, air conditioning, ample space. For when you need extra space with a budget friendly ride.",
-      city: "2600",
-      outstation: "18",
-      airport: "1800",
+      city: "5000",
+      outstation: "22",
+      airport: "2000",
     },
     {
       id: 5,
       image: "../images/bus.png",
       name: "Bus",   
       paragraph:
-      "56 seater, air-conditioned, top rated drivers, spacious, extra seats. Enjoy extra sple boot space with most trusted drivers.",
+      "49 seater, air-conditioned, top rated drivers, spacious, extra seats. Enjoy extra sple boot space with most trusted drivers.",
       city: "-",
-      outstation: "35",
+      outstation: "60",
       airport: "5000",
     },
   ],
