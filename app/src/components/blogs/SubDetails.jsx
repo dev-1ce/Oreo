8e04de892a9afd2f88b53e87378ed930632468cd
dev-1ce <141261042+dev-1ce@users.noreply.github.com>
2024-02-16 17:34:33 +0530

    fix timing made it into table (#47)

--- app/src/components/blogs/SubDetails.jsx
+++ app/src/components/blogs/SubDetails.jsx
@@ -7,10 +7,30 @@ function SubDetails({ subDetails }) {
       <h2 className="font-bold font-20 mt-2 text-brown">{subDetails.heading}</h2>
       <h3 className="font-bold font-17 mt-2 text-brown">{subDetails.question}</h3>
       {subDetails.image && (<img src={subDetails.image} className="img-fluid my-3"></img>)}
-      {subDetails.paragraph.map((subParagraghDetails, index) => {
+      {subDetails.paragraph && subDetails.paragraph.map((subParagraghDetails, index) => {
         return <p key={index} className="font-regular font-16 text-brown">{subParagraghDetails}</p>;
       })
       }
+      {subDetails.table && (
+        <table className="table">
+          <thead>
+            <tr>
+              {subDetails.table.headers.map((header, index) => (
+                <th key={index}>{header}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {subDetails.table.rows.map((row, rowIndex) => (
+              <tr key={rowIndex}>
+                {row.map((cell, cellIndex) => (
+                  <td key={cellIndex}>{cell}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
       
     </React.Fragment>
   );
